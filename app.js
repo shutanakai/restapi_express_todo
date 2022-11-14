40316984837bd3d7fd1d83c094a4f13cf693a33c
*93kC8O4PqJ <shuta.nakai@hakuhodo-ms.co.jp>
2022-11-14 11:20:42 +0900

    CORS対応

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const taskRoutes = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
 
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:4000',
+  credentials: true, //レスポンスヘッダーにAccess-Control-Allow-Credentials追加
+  optionsSuccessStatus: 200 //レスポンスstatusを200に設定
+}))
 
 const PORT = 3000;
 
